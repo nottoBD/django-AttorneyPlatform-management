2c6f7336f4a53550493bebd8f117377bb65e4be2
David Botton <david.botton@mail.com>
2024-07-03 13:18:13 +0200

    Ajout traduction au toast de réinitialisation du mdp

--- accounts/views.py
+++ accounts/views.py
@@ -229,10 +229,9 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
     template_name = 'registration/password_reset.html'
     email_template_name = 'registration/password_reset_mail.html'
     subject_template_name = 'registration/password_reset_subject.txt'
-    success_message = "We've emailed you instructions for setting your password, " \
-                      "if an account exists with the email you entered. You should receive them shortly. " \
-                      "If you don't receive an email, " \
-                      "please make sure you've entered the address you registered with, and check your spam folder."
+    success_message = _("We've emailed you instructions for setting your password, if an account exists with the "
+                        "email you entered. You should receive them shortly. If you don't receive an email, "
+                        "please make sure you've entered the address you registered with, and check your spam folder.")
     success_url = reverse_lazy('home')
 
 
