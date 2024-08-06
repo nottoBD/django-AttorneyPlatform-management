4cffec36d2cefc15858063f5b867a3fcbe443a02
Arnaud Mahieu <94074921+Serenity1909@users.noreply.github.com>
2024-08-06 21:46:14 +0200

    fix add child for testing

--- payments/forms.py
+++ payments/forms.py
@@ -23,6 +23,7 @@ from datetime import date
 from django import forms
 from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
+from django.utils import timezone
 
 from accounts.views import User
 from .models import Document, Case, Category, Child
@@ -192,16 +193,23 @@ class ChildForm(forms.ModelForm):
             )
         ]
     )
+    case = forms.ModelChoiceField(queryset=Case.objects.all(), widget=forms.HiddenInput())
 
     class Meta:
         model = Child
-        fields = ['first_name', 'last_name', 'birth_date']
+        fields = ['first_name', 'last_name', 'birth_date', 'case']
         widgets = {
             'birth_date': forms.DateInput(attrs={'type': 'date'})
         }
 
+    def __init__(self, *args, **kwargs):
+        case_id = kwargs.pop('case_id', None)
+        super().__init__(*args, **kwargs)
+        if case_id is not None:
+            self.fields['case'].initial = case_id
+
     def clean_birth_date(self):
         birth_date = self.cleaned_data.get('birth_date')
-        if birth_date and birth_date > date.today():
+        if birth_date and birth_date > timezone.now().date():
             raise ValidationError('La date de naissance ne peut pas être postérieure à aujourd\'hui.')
         return birth_date
